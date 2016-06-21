b68f335611c29fe0eabafbe9c0fcd475b84a697c
syarbeat <srf.hidayat@gmail.com>
2016-06-21 13:26:21 +0700

    add monthy & yearly statistic chart

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,58 +1,104 @@
-/**
- * Created by Asus X455LA on 20/06/2016.
- */
 
 var GenerateCharts = function() {
 
-    var initPieChart = function() {
+    var initMonthlyStatisticChart = function() {
         var source =
         {
             datatype: "csv",
             datafields: [
-                { name: 'Browser' },
-                { name: 'Share' }
+                { name: 'Tanggal' },
+                { name: 'Hit' }
+
             ],
-            url: 'http://localhost:8080/BMKG/jqwidgets/demos/sampledata/desktop_browsers_share_dec2011.txt'
+            url: 'http://localhost:8080/BMKG/statistik_monthly.txt'
         };
         var dataAdapter = new $.jqx.dataAdapter(source, { async: false, autoBind: true, loadError: function (xhr, status, error) { alert('Error loading "' + source.url + '" : ' + error); } });
         // prepare jqxChart settings
         var settings = {
-            title: "",
+            title: "Monthly Statistic",
             description: "",
-            enableAnimations: true,
             showLegend: true,
-            showBorderLine: true,
-            legendLayout: { left: 5, top: 5, width: 300, height: 200, flow: 'vertical' },
+            enableAnimations: true,
             padding: { left: 5, top: 5, right: 5, bottom: 5 },
-            titlePadding: { left: 0, top: 0, right: 0, bottom: 10 },
+            titlePadding: { left: 90, top: 0, right: 0, bottom: 10 },
             source: dataAdapter,
-            colorScheme: 'scheme03',
+            xAxis:
+            {
+                dataField: 'Tanggal',
+                gridLines: { visible: true },
+                valuesOnTicks: false
+            },
+            colorScheme: 'scheme01',
+            columnSeriesOverlap: false,
             seriesGroups:
                 [
                     {
-                        type: 'pie',
-                        showLabels: true,
-                        series:
-                            [
-                                {
-                                    dataField: 'Share',
-                                    displayText: 'Browser',
-                                    labelRadius: 170,
-                                    initialAngle: 15,
-                                    radius: 80,
-                                    centerOffset: 0,
-                                    formatFunction: function (value) {
-                                        if (isNaN(value))
-                                            return value;
-                                        return parseFloat(value) + '%';
-                                    },
-                                }
-                            ]
+                        type: 'column',
+                        valueAxis:
+                        {
+                            visible: true,
+                            unitInterval: 100,
+                            title: { text: 'Statistic<br>' }
+                        },
+                        series: [
+                            { dataField: 'Hit', displayText: 'Hit count' }
+
+                        ]
                     }
                 ]
         };
         // setup the chart
-        $('#chartContainer').jqxChart(settings);
+        $('#monthlystatistic').jqxChart(settings);
+    }
+
+    var initYearlyStatisticChart = function() {
+        var source =
+        {
+            datatype: "csv",
+            datafields: [
+                { name: 'Bulan' },
+                { name: 'Hit' }
+
+            ],
+            url: 'http://localhost:8080/BMKG/statistik_yearly.txt'
+        };
+        var dataAdapter = new $.jqx.dataAdapter(source, { async: false, autoBind: true, loadError: function (xhr, status, error) { alert('Error loading "' + source.url + '" : ' + error); } });
+        // prepare jqxChart settings
+        var settings = {
+            title: "Yearly Statistic",
+            description: "",
+            showLegend: true,
+            enableAnimations: true,
+            padding: { left: 5, top: 5, right: 5, bottom: 5 },
+            titlePadding: { left: 90, top: 0, right: 0, bottom: 10 },
+            source: dataAdapter,
+            xAxis:
+            {
+                dataField: 'Bulan',
+                gridLines: { visible: true },
+                valuesOnTicks: false
+            },
+            colorScheme: 'scheme01',
+            columnSeriesOverlap: false,
+            seriesGroups:
+                [
+                    {
+                        type: 'column',
+                        valueAxis:
+                        {
+                            visible: true,
+                            unitInterval: 1000,
+                            title: { text: 'Statistic<br>' }
+                        },
+                        series: [
+                            { dataField: 'Hit', displayText: 'Hit count' }
+
+                        ]
+                    }
+                ]
+        };
+        // setup the chart
+        $('#yearlystatistic').jqxChart(settings);
     }
 
     return {
@@ -60,8 +106,8 @@ var GenerateCharts = function() {
 
         init: function() {
 
-            initPieChart();
-
+            initMonthlyStatisticChart();
+            initYearlyStatisticChart();
         }
 
     };
