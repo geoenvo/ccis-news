2dd68f9971a44a48767edce04bd45f9f37542629
starmizi <said.tarmizi@outlook.com>
2017-06-14 11:53:15 +0700

    3 row

--- mysql.php
+++ mysql.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT title, url, thumbnail, date_str, description FROM open_news_article";
+$sql = "SELECT title, url, thumbnail, date_str, description, categories, published FROM open_news_article";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
