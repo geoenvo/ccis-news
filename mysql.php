143ecaaef03d43928b199c3be650678618401fc0
starmizi <said.tarmizi@outlook.com>
2017-06-14 11:52:06 +0700

    3 row

--- mysql.php
+++ mysql.php
@@ -18,16 +18,20 @@ if ($result->num_rows > 0) {
     for($i=$result->num_rows-3; $i < $result->num_rows; $i++) {
         echo '<div class="row">';
         while($row = $result->fetch_assoc()) {
-            echo '<div class="col-md-4 portfolio-item">';
-            echo '<a href="#">';
-            echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
-            echo '</a>';
-            echo '<h5>';
-            echo '<a href="'.$row['url'].'">'.$row['title'].'</a><br>';
-            echo '</h5>';
-            echo '<h6>'.$row['date_str'].'</h6>';
-            echo ''.$row['description'].' <br>';
-            echo '</div>';
+            if ($row['published'] == 1 && $row['thumbnail']) {
+                echo '<div class="col-md-4 portfolio-item">';
+                echo '<a href="#">';
+                echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
+                echo '</a>';
+                echo '<h5>';
+                echo '<a href="'.$row['url'].'">'.$row['title'].'</a><br>';
+                echo '</h5>';
+                echo '<h6>'.$row['date_str'].'</h6>';
+                echo ''.$row['description'].' <br>';
+                echo '</div>';
+            }
+            else {
+            }
         }
         echo '</div>';
         echo '<hr style="color:black;" />';
