b082886f1092d1c2320d91caf5f60ce7cc8f7869
starmizi <said.tarmizi@outlook.com>
2017-06-14 09:13:20 +0700

    3 row

--- mysql.php
+++ mysql.php
@@ -16,8 +16,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo '<div class="row">';
-    $row = $result->fetch_assoc();
-    for($i=$index-3; $i < $maxindex; $i++) {
+    while($row = $result->fetch_assoc()) {
         echo '<div class="col-md-4 portfolio-item">';
         echo '<a href="#">';
         echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
