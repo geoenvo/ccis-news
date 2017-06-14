fe8c27d35270bc93983ece839e19f2494f0cedb2
starmizi <said.tarmizi@outlook.com>
2017-06-14 09:57:25 +0700

    3 row

--- index.php
+++ index.php
@@ -443,37 +443,42 @@ $temp_media_share = $statistic->getMediaShareData("mediashare.txt");
                             </div>
                             <div class="panel-body">
 
-                            	<?php
-                            		$servername = "localhost";
-									$username = "dds_readonly";
-									$password = "ddsreadonlyp455w0rd";
-									$dbname = "dds";
-									try {
-									    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-									    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-									    $stmt = $conn->prepare("SELECT title, url, thumbnail, date_str, description FROM open_news_article"); 
-									    $stmt->execute();
-									    echo '<div class="row">';
-										while ($row = $result->fetch_assoc()) {
-            								echo '<div class="col-md-4 portfolio-item">';
-            								echo '<a href="#">';
-            								echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
-            								echo '</a>';
-            								echo '<h5>';
-            								echo '<a href="'.$row['url'].'">'.$row['title'].'</a><br>';
-            								echo '</h5>';
-            								echo '<h6>'.$row['date_str'].'</h6>';
-            								echo ''.$row['description'].' <br>';
-            								echo '</div>';
-										}
-										echo '</div>';
-        								echo '<hr style="color:black;" />';
-									}
-									catch(PDOException $e) {
-									    echo "Error: " . $e->getMessage();
-									}
-									$conn = null; 
-                            	?>
+								<?php
+								$servername = "localhost";
+								$username = "dds_readonly";
+								$password = "ddsreadonlyp455w0rd";
+								$dbname = "dds";
+								// Create connection
+								$conn = new mysqli($servername, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+								    die("Connection failed: " . $conn->connect_error);
+								} 
+								$sql = "SELECT title, url, thumbnail, date_str, description FROM open_news_article";
+								$result = $conn->query($sql);
+								if ($result->num_rows > 0) {
+								    for($i=$result->num_rows-3; $i < $result->num_rows; $i++) {
+								        echo '<div class="row">';
+								        while($row = $result->fetch_assoc()) {
+								            echo '<div class="col-md-4 portfolio-item">';
+								            echo '<a href="#">';
+								            echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
+								            echo '</a>';
+								            echo '<h5>';
+								            echo '<a href="'.$row['url'].'">'.$row['title'].'</a><br>';
+								            echo '</h5>';
+								            echo '<h6>'.$row['date_str'].'</h6>';
+								            echo ''.$row['description'].' <br>';
+								            echo '</div>';
+								        }
+								        echo '</div>';
+								        echo '<hr style="color:black;" />';
+								    }
+								} else {
+								    echo "0 results";
+								}
+								$conn->close();
+								?>
 
                                 <?php
                                 /*
