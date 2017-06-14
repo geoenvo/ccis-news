c204006ce37aa0a401a657d81532422a3b298f2b
starmizi <said.tarmizi@outlook.com>
2017-06-14 09:43:13 +0700

    3 row

--- mysql.php
+++ mysql.php
@@ -15,21 +15,23 @@ $sql = "SELECT title, url, thumbnail, date_str, description FROM open_news_artic
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo '<div class="row">';
-    while($row = $result->fetch_assoc()) {
-        echo '<div class="col-md-4 portfolio-item">';
-        echo '<a href="#">';
-        echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
-        echo '</a>';
-        echo '<h5>';
-        echo '<a href="'.$row['url'].'">'.$row['title'].'</a><br>';
-        echo '</h5>';
-        echo '<h6>'.$row['date_str'].'</h6>';
-        echo ''.$row['description'].' <br>';
+    for($i=$result->num_rows-3; $i < $result->num_rows; $i++) {
+        echo '<div class="row">';
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="col-md-4 portfolio-item">';
+            echo '<a href="#">';
+            echo '<img src="http://139.162.55.216:8000/open_news/thumbnails_full/?thumbnail='.$row['thumbnail'].'" width="350" height="200"  alt="">';
+            echo '</a>';
+            echo '<h5>';
+            echo '<a href="'.$row['url'].'">'.$row['title'].'</a><br>';
+            echo '</h5>';
+            echo '<h6>'.$row['date_str'].'</h6>';
+            echo ''.$row['description'].' <br>';
+            echo '</div>';
+        }
         echo '</div>';
+        echo '<hr style="color:black;" />';
     }
-    echo '</div>';
-    echo '<hr style="color:black;" />';
 } else {
     echo "0 results";
 }
