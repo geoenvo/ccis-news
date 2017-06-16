62c9bbf157baedfe80e736daca180227ae5cd11a
starmizi <said.tarmizi@outlook.com>
2017-06-16 14:17:10 +0700

    Add twitter template

--- index.php
+++ index.php
@@ -537,6 +537,22 @@ $temp_media_share = $statistic->getMediaShareData("mediashare.txt");
                         </div>
                     </section>
                 </div> <!-- THE END OF THE SECOND ROW OF THE MAIN CONTENT-->
+                
+                <script>window.twttr = (function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0],
+                    t = window.twttr || {};
+                  if (d.getElementById(id)) return t;
+                  js = d.createElement(s);
+                  js.id = id;
+                  js.src = "https://platform.twitter.com/widgets.js";
+                  fjs.parentNode.insertBefore(js, fjs);
+                  t._e = [];
+                  t.ready = function(f) {
+                    t._e.push(f);
+                  };
+                  return t;
+                  }(document, "script", "twitter-wjs"));
+                </script>
 
                 <div class="row">
                     <section id="tweetaggregator">
