6d97ad5d074f1ecf46d10d77914bffc27ed96060
starmizi <said.tarmizi@outlook.com>
2017-08-03 18:39:48 +0700

    Create tweet.php

---
+++ inc/tweet.php
@@ -0,0 +1,61 @@
+<?php
+include 'inc/pgsql.php';
+echo '<div class="row">';
+echo '<div class="col-md-3">';
+echo '<button class="twitter-hashtag-button">#PerubahanIklim</button>';
+$x=0;
+foreach ($connec->query($sql) as $row) {
+    if ($row['categories'] == 'perubahan iklim') {
+        echo '<blockquote class="twitter-tweet">';
+        echo '<a href="https://twitter.com/'.$row['user_screen_name'].'';
+        echo '/status/'.$row['tweet_id'].'">';
+        echo '</a></blockquote>';
+        $x++;
+        if ($x == 2) {break;}
+    }
+}
+echo '</div>';
+echo '<div class="col-md-3">';
+echo '<button class="twitter-hashtag-button">#ClimateChange</button>';
+$x=0;
+foreach ($connec->query($sql) as $row) {
+    if ($row['categories'] == 'climate change') {
+        echo '<blockquote class="twitter-tweet">';
+        echo '<a href="https://twitter.com/'.$row['user_screen_name'].'';
+        echo '/status/'.$row['tweet_id'].'">';
+        echo '</a></blockquote>';
+        $x++;
+        if ($x == 2) {break;}                                        
+    }
+}
+echo '</div>';
+echo '<div class="col-md-3">';
+echo '<button class="twitter-hashtag-button">#KualitasUdara</button>';
+$x=0;
+foreach ($connec->query($sql) as $row) {
+    if ($row['categories'] == 'kualitas udara') {
+        echo '<blockquote class="twitter-tweet">';
+        echo '<a href="https://twitter.com/'.$row['user_screen_name'].'';
+        echo '/status/'.$row['tweet_id'].'">';
+        echo '</a></blockquote>';
+        $x++;
+        if ($x == 2) {break;}   
+    }
+}
+echo '</div>';
+echo '<div class="col-md-3">';
+echo '<button class="twitter-hashtag-button">#ClimateChangeAdaptation</button>';
+$x=0;
+foreach ($connec->query($sql) as $row) {
+    if ($row['categories'] == 'climate change adaptation') {
+        echo '<blockquote class="twitter-tweet">';
+        echo '<a href="https://twitter.com/'.$row['user_screen_name'].'';
+        echo '/status/'.$row['tweet_id'].'">';
+        echo '</a></blockquote>';
+        $x++;
+        if ($x == 2) {break;}   
+    }
+}
+echo '</div>';
+echo '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+?>
